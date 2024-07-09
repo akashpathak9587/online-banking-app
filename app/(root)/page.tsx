09270137e63cf420646cc5923ae767421b72eee4
Akash Pathak <akashpathak@Akashs-MacBook-Air.local>
2024-07-09 17:33:31 +0530

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/user.actions'
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accountsData = []
+  const accounts = {totalBanks: 1, totalCurrentBalance: 10000}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type='greeting' title='Welcome' user={loggedIn?.name || 'Guest'} subtext='Access and manage your account and transactions efficiently.' />
+          <TotalBalanceBox accounts={accountsData} totalBanks={accounts?.totalBanks} totalCurrentBalance={accounts?.totalCurrentBalance} />
+        </header>
+        {/* <RecentTransaction /> */}
+      </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[{currentBalance: 20000}, {currentBalance: 10000}]} />
+    </section>
   )
 }
 
